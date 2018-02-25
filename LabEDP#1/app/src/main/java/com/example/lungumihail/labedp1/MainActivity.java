be770017be0d4d0fa725edf189285c51f0b501cf
Lungu Mihail <lungumihail@Lungus-MacBook-Pro.local>
2018-02-25 21:02:56 +0200

    New edit bug

--- LabEDP#1/app/src/main/java/com/example/lungumihail/labedp1/MainActivity.java
+++ LabEDP#1/app/src/main/java/com/example/lungumihail/labedp1/MainActivity.java
@@ -3,10 +3,10 @@ package com.example.lungumihail.labedp1;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,19 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         Button customButton = (Button) findViewById(R.id.customButton);
         final TextView customText = (TextView) findViewById(R.id.customText);
-
-
+        Button defaultButton = (Button) findViewById(R.id.defaultButton);
+        final TextView defaultText = (TextView) findViewById(R.id.defaultText);
+        final EditText defaultEdit = (EditText) findViewById(R.id.defaultEdit);
         customButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 customText.setTextSize(15);
+                defaultText.setTextColor(0xFF0000);
+
+
             }
         });
+        defaultButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                defaultText.setTextColor(0xFF00FF00);
+                customText.setTextSize(40);
+                defaultEdit.setTextColor(0xFF0000);
+            }
+        });
+
+
+
 
     }
 

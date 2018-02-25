eb9cbfbd6b417de11339da52263d86034d01d264
Lungu Mihail <lungumihail@Lungus-MacBook-Pro.local>
2018-02-25 19:44:45 +0200

    ADDED MMAIN ACTIVI

--- LabEDP#1/app/src/main/java/com/example/lungumihail/labedp1/MainActivity.java
+++ LabEDP#1/app/src/main/java/com/example/lungumihail/labedp1/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.lungumihail.labedp1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Button customButton = (Button) findViewById(R.id.customButton);
+        final TextView customText = (TextView) findViewById(R.id.customText);
+
+
+        customButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customText.setTextSize(15);
+            }
+        });
+
     }
+
+
+
+
+
+
+
 }
+
+
